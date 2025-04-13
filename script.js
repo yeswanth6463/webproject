34a786c3ea57c374cfda8d1dbbf6517674ff6581
YESWANTH <113580288+yeswanth6463@users.noreply.github.com>
2025-04-13 18:05:14 +0530

    commit

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const loginButton = document.getElementById('login-button');
-          const loginForm = document.querySelector('.login-form');
-          const registerButton = document.getElementById('register');
-          const registerForm = document.querySelector('.Register-form');
+const loginForm = document.querySelector('.login-form');
+const registerButton = document.getElementById('register');
+const registerForm = document.querySelector('.Register-form');
 
-          loginButton.addEventListener('click', () => {
-            loginForm.classList.toggle('show');
-            registerForm.classList.remove('show');
-          });
-          registerButton.addEventListener('click', () => {
-            registerForm.classList.toggle('show');
-            loginForm.classList.remove('show');
-          });
+loginButton.addEventListener('click', () => {
+  loginForm.classList.toggle('show');
+  registerForm.classList.remove('show');
+});
+
+registerButton.addEventListener('click', () => {
+  registerForm.classList.toggle('show');
+  loginForm.classList.remove('show');
+});
+
+// Initialize ScrollReveal with common configuration
+const sr = ScrollReveal({
+  distance: '80px',
+  duration: 2000,
+  delay: 200,
+  reset: false
+});
+
